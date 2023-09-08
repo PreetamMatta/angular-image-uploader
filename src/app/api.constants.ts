37804a8d5083aaf391d7a2c8e21770894ac5ed53
PreetamMatta <preetam.matta@gmail.com>
2023-09-08 00:16:21 -0400

    Added fetch button

--- src/app/api.constants.ts
+++ src/app/api.constants.ts
@@ -1,4 +1,5 @@
 export const API_URLS = {
-  get: 'https://jsonplaceholder.typicode.com/users',
+  get: 'https://o3hde1m4ud.execute-api.us-east-1.amazonaws.com/Dev/kubex-identity-doc-poc/identity-output-file/Californian_sample_driver.json',
   put: 'https://gbcjdqld06.execute-api.us-east-1.amazonaws.com/Dev/kubex-identity-doc-poc/identity-input-file/Californian_sample_driver.jpg',
+  // put: 'https://k8xg4yzgbk.execute-api.us-east-1.amazonaws.com/dev1',
 };
